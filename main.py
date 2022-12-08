a457e07cdbf32bb87497d323c965d9d7c5be1083
Syahrul Hidayat <syah.hidayat71@gmail.com>
2022-12-08 09:55:01 +0700

    Has same functionality with the real tetris game

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import sys
 import cv2
 import time
+import random
+
 from datetime import datetime
 
 import util.screen as sc
@@ -24,8 +26,7 @@ def main():
 
     board = Board(WINDOW_SIZE, PADDING, BOARD, BLOCK_SIZE)
 
-    block = Block()
-    point = Point(board.board_w//2-1, 0)
+    block = Block(board.board_w)
 
     is_game_over = False
     is_stuck = False
@@ -34,31 +35,31 @@ def main():
 
     score = 0
     while True:
-        sc.clear_screen()
-        print(f'Score: {score}')
-        print(f'Time: {str(datetime.now() - time_start).split(".")[0]}')
+        # sc.clear_screen()
+        # print(f'Score: {score}')
+        # print(f'Time: {str(datetime.now() - time_start).split(".")[0]}')
 
         if is_stuck:
-            if point.y == 0:
+            if block.point.y == 0:
                 # Game Over
                 is_game_over = True
                 break
+            
             score += block.score
             is_stuck = False
-            board.submit(block, point)
-            point = Point(board.board_w // 2 - 1, 0)
-            block = Block()
+            board.submit(block, block.point)
+            block = Block(board.board_w)
 
 
         # Put the block
-        values, point = board.put_block(block, point)
+        values = board.put_block(block, block.point)
         if values is None:
             # Stuck
             is_stuck = True
             continue
 
-        point_projection = board.get_projection_point(block, point)
-        values, _ = board.put_block(block, point_projection, values_projection=values)
+        point_projection = board.get_projection_point(block, block.point)
+        values = board.put_block(block, point_projection, values_projection=values)
         if values is None:
             # Stuck
             print('Stuck')
@@ -68,25 +69,27 @@ def main():
         img = board.draw_board(value_temp=values)
 
         # # Draw "point" coordinate
-        # xc, yc = PADDING_X + point.x * BLOCK_SIZE_W + BLOCK_SIZE_W//2, PADDING_Y + point.y * BLOCK_SIZE_H + BLOCK_SIZE_H//2
+        # xc, yc = PADDING_X + block.point.x * BLOCK_SIZE_W + BLOCK_SIZE_W//2, PADDING_Y + block.point.y * BLOCK_SIZE_H + BLOCK_SIZE_H//2
         # img = cv2.circle(img, (xc, yc), BLOCK_SIZE_W//4, (255, 0, 0), -1)
         #
         # # Draw "block shape"
-        # x1, y1 = PADDING_X + point.x * BLOCK_SIZE_W, PADDING_Y + point.y * BLOCK_SIZE_H
-        # x2, y2 = PADDING_X + (point.x + block.width) * BLOCK_SIZE_W, PADDING_Y + (point.y + block.height) * BLOCK_SIZE_H
+        # x1, y1 = PADDING_X + block.point.x * BLOCK_SIZE_W, PADDING_Y + block.point.y * BLOCK_SIZE_H
+        # x2, y2 = PADDING_X + (block.point.x + block.width) * BLOCK_SIZE_W, PADDING_Y + (block.point.y + block.height) * BLOCK_SIZE_H
         # img = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
         cv2.imshow('Tetris', img)
 
+        # Gravity
         while True:
             key = cv2.waitKey(10)
             if key != -1:
                 break
+                
             if (datetime.now() - time_past).total_seconds() > 1:
                 time_past = datetime.now()
-                point.move_down()
-                if board.is_block_overlap(block, point):
-                    point.move_up()
+                block.point.move_down()
+                if board.is_block_overlap(block, block.point):
+                    block.point.move_up()
                     is_stuck = True
                 break
 
@@ -96,57 +99,99 @@ def main():
         if key == ord('q'):
             break
 
-        # Change block type
+        # Drop the block
         if key == ord(' ') and not is_game_over:
-            # Drop
-            point = board.get_projection_point(block, point)
+            # # Drop
+            block.point = point_projection
             is_stuck = True
+
+            # # Only change the block (DEBUG)
+            # block = Block(board.board_w)
             continue
 
         # Rotate: Clockwise
         if key == ord('r') and not is_game_over:
             # Check if rotating causes overlap
             block.rotate(clockwise=True)
-            if board.is_block_overlap(block, point):
-                print('Overlap: should be game over!')
-                block.rotate(clockwise=False)
+            # print(f'block is rotated clockwise')
+            p = Point(block.point.x, block.point.y)
+            if board.is_block_overlap(block, p, verbose=True):
+                # print(f'block is overlap, trying to move left')
+                # try to move left 
+                p.move_left()
+                # print(f'block is moved left')
+                if board.is_block_overlap(block, p, verbose=True):
+                    # print(f'block is still overlap, trying to move left again')
+                    # try to move left 
+                    p.move_left()
+                    # print(f'block is moved left again')
+                    if board.is_block_overlap(block, p, verbose=True):
+                        # print(f'block is still overlap, restore to original position')
+                        # rotate and left is still overlap, return to original 
+                        p.move_right()
+                        p.move_right()
+                        # print(f'block is restored to original, trying to move fight')
+
+                        # try to move right 
+                        p.move_right()
+                        # print(f'block is moved right')
+                        if board.is_block_overlap(block, p, verbose=True):
+                            # print(f'block is still overlap, trying to move right again')
+                            # try to move left 
+                            p.move_right()
+                            # print(f'block is moved right again')
+                            if board.is_block_overlap(block, p, verbose=True):
+                                # print(f'block is still overlap, restore to original position')
+                                # rotate and right is still overlap, return to original 
+                                p.move_left()
+                                p.move_left()
+                                # print(f'block is restored to original, game over!')
+                                block.rotate(clockwise=False)
+            # print()
+            block.point = Point(p.x, p.y)
 
         # Rotate: CounterClockwise
         if key == ord('e') and not is_game_over:
             block.rotate(clockwise=False)
-            if board.is_block_overlap(block, point):
+            if board.is_block_overlap(block, block.point, verbose=True):
                 print('Overlap: should be game over!')
                 block.rotate(clockwise=True)
 
         # Arrow: left
-        p = Point(point.x, point.y)
+        p = Point(block.point.x, block.point.y)
         if (key == 81 or key == ord('a')) and not is_game_over:
             p.move_left()
-            if not board.is_block_overlap(block, p):
-                point.move_left()
 
-        # # Arrow: up
-        # if key == 82 and not is_game_over:
-        #     p.move_up()
-        #     if not board.is_block_overlap(block, p):
-        #         point.move_up()
+            if not board.is_block_overlap(block, p, verbose=True):
+                block.point.move_left()
+            else:
+                print('Cannot move left!')
 
         # Arrow: right
         if (key == 83 or key == ord('d')) and not is_game_over:
             p.move_right()
-            if not board.is_block_overlap(block, p):
-                point.move_right()
+            if not board.is_block_overlap(block, p, verbose=True):
+                block.point.move_right()
+            else:
+                print('Cannot move right!')
+
+        # Arrow: up
+        if (key == 82 or key == ord('w')) and not is_game_over:
+            p.move_up()
+            if not board.is_block_overlap(block, p, verbose=True):
+                block.point.move_up()
 
         # Arrow: bottom
         if (key == 84 or key == ord('s')) and not is_game_over:
             p.move_down()
-            if not board.is_block_overlap(block, p):
-                point.move_down()
+            if not board.is_block_overlap(block, p, verbose=True):
+                block.point.move_down()
 
     if is_game_over:
         print('Game Over')
     else:
         print('Exiting the game')
+    
     sys.exit(0)
 
 
--- model/board.py
+++ model/board.py
@@ -20,6 +20,8 @@ def is_overlap(m1, m2):
 
 class Board:
 
+    REWARD = 10
+
     def __init__(self, window_size, padding, board, block_size):
         self.window_w, self.window_h = window_size
         self.padding_w, self.padding_h = padding
@@ -33,7 +35,7 @@ class Board:
     def create_board(self):
         return np.array([[0 for x in range(self.board_w)] for y in range(self.board_h)], dtype=np.int8)
 
-    def create_background(self):
+    def create_background(self, show_line=False):
         # Background
         background = np.zeros((self.window_h, self.window_w, 3), dtype=np.int8)
 
@@ -42,14 +44,15 @@ class Board:
         x2, y2 = self.window_w - self.padding_w, self.window_h - self.padding_h
         background = cv2.rectangle(background, (x1, y1), (x2, y2), (0, 0, 255), 4)
 
-        # # Line Separator
-        # for i in range(self.board_w):
-        #     x = self.padding_w + self.block_w * i
-        #     background = cv2.line(background, (x, y1), (x, y2), (0, 0, 255), 1)
-        #
-        # for i in range(self.board_h):
-        #     y = self.padding_h + self.block_h * i
-        #     background = cv2.line(background, (x1, y), (x2, y), (0, 0, 255), 1)
+        if show_line:
+            # Line Separator
+            for i in range(self.board_w):
+                x = self.padding_w + self.block_w * i
+                background = cv2.line(background, (x, y1), (x, y2), (0, 0, 255), 1)
+            
+            for i in range(self.board_h):
+                y = self.padding_h + self.block_h * i
+                background = cv2.line(background, (x1, y), (x2, y), (0, 0, 255), 1)
 
         return background
 
@@ -73,14 +76,34 @@ class Board:
 
         return img
 
-    def is_block_overlap(self, block, point):
+    def is_block_overlap(self, block, point, verbose=False):
         values = self.value.copy()
 
         # Check if block is overlap
-        value = np.copy(values[point.y:point.y+block.height, point.x:point.x+block.width])
+        h_s, h_e, w_s, w_e = point.y, min(self.board_h, point.y + block.height), max(0, point.x), min(values.shape[1], point.x + block.width)
+        value = np.copy(values[h_s:h_e, w_s:w_e])
         block_i = np.copy(block.get_identity())
 
+        # Is overlap with the edge
         if value.shape != block_i.shape:
+            if max(0, point.x) == 0:
+                if verbose: print('Block is on the left of the edge!')
+                if (np.sum(block_i[:, :(-point.x)]) == 0) and (np.sum(value[:, (-point.x):]) == 0):
+                    # if verbose: print('but its oke')
+                    return False
+                else:
+                    # if verbose: print('but its not oke')
+                    pass
+
+            if min(values.shape[1], point.x + block.width) == values.shape[1]:
+                if verbose: print('Block is on the right of the edge!')
+                if (np.sum(block_i[:, -(point.x + block.width - values.shape[1]):]) == 0) and (np.sum(value[:, :-(point.x + block.width - values.shape[1])]) == 0): # and is_overlap(value, block_i[:, :-1]):
+                    # if verbose: print('but its oke')
+                    return False
+                else:
+                    # if verbose: print('but its not oke')
+                    pass
+                
             return True
 
         return is_overlap(value, block_i)
@@ -89,37 +112,88 @@ class Board:
         values = self.value.copy()
         if values_projection is not None:
             values = values_projection
-
-        # Can not go outside the frame
-        if point.x < 0:
-            point.x = 0
-        if point.y < 0:
-            point.y = 0
-
-        if point.x > self.board_w - block.width:
-            point.x = self.board_w - block.width
-        if point.y > self.board_h - block.height:
-            point.y = self.board_h - block.height
-
-        values_id = np.copy(values[point.y:point.y+block.height, point.x:point.x+block.width])
-        values_id[values_id > 0] = 1
-        is_overlap_ = is_overlap(values_id, block.get_identity())
-        if values_projection is not None:
-            values[point.y:point.y + block.height, point.x:point.x + block.width]\
-                += block.get_identity() * (len(Block.BLOCKS) - 1)
+            color = 8
         else:
-            if not is_overlap_:
-                values[point.y:point.y+block.height, point.x:point.x+block.width] += block.value
-            else:
-                # Stuck
-                return None, point
+            color = block.id
 
-        # Do not overlap with the projection
-        values[values > len(Block.BLOCKS) - 1] -= len(Block.BLOCKS) - 1
+        w_s, w_e = max(0, point.x), min(self.board_w, point.x + block.width)
+        h_s, h_e = max(0, point.y), min(self.board_h, point.y + block.height)
+        block_identity = block.get_identity()
 
-        return values, point
+        # Not overlap with the projection
+        if (w_e - w_s) != block.width:
+            # print('Overlap with side')
+            # print((h_s, h_e, w_s, w_e, block_identity, len(Block.BLOCKS) - 1, ))
+
+            if w_e <= point.x + block.width - 1:
+                # print(f'{w_e} == {point.x} + {block.width} - 1')
+                values[h_s:h_e, w_s:w_e] += (block_identity * color)[:, :(block.width - (point.x + block.width - w_e))]
+                
+            if w_s == 0:
+                # print(f'{w_s}')
+                values[h_s:h_e, w_s:w_e] += (block_identity * color)[:, (block.width - (w_e - w_s)):]
+                
+        else:
+            values[h_s:h_e, w_s:w_e] += block_identity * color
+
+        values[values > len(Block.BLOCKS) - 1] = len(Block.BLOCKS) - 1
+        # values[values > len(Block.BLOCKS) - 1] -= len(Block.BLOCKS) - 1
+
+
+        # if not is_projection:
+        #     self.value = values
+
+        # # Can not go outside the frame
+        # if point.x < -1:
+        #     point.x = -1
+        # if point.y < 0:
+        #     point.y = 0
+
+        # if point.x > self.board_w - block.width + 1:
+        #     point.x = self.board_w - block.width + 1
+        # if point.y > self.board_h - block.height:
+        #     point.y = self.board_h - block.height
+
+        # values_id = np.copy(values[point.y:point.y+block.height, point.x:point.x+block.width])
+        # values_id[values_id > 0] = 1
+        # if values_projection is not None:
+        #     if max(-1, point.x) == -1 or min(values.shape[1] + 1, point.x+block.width) == values.shape[1] + 1:
+        #         if max(-1, point.x) == -1:
+        #             if np.sum(block.value[:, 0]) == 0:
+        #                 values[point.y:point.y+block.height, max(0, point.x):min(values.shape[1]+1, point.x+block.width)] += (block.get_identity() * (len(Block.BLOCKS) - 1))[:, 1:]
+
+        #         if min(values.shape[1] + 1, point.x+block.width) == values.shape[1]+1:
+        #             if np.sum(block.value[:, -1]) == 0:
+        #                 values[point.y:point.y+block.height, max(-1, point.x):min(values.shape[1]+1, point.x+block.width)] += (block.get_identity() * (len(Block.BLOCKS) - 1))[:, :-1]
+        #     else:
+        #         values[point.y:point.y+block.height, point.x:point.x + block.width] += block.get_identity() * (len(Block.BLOCKS) - 1)
+
+        # else:
+        #     is_overlap_ = False # is_overlap(values_id, block.get_identity())
+            
+        #     if max(-1, point.x) == -1 or min(values.shape[1] + 1, point.x+block.width) == values.shape[1] + 1:
+        #         if max(-1, point.x) == -1:
+        #             if np.sum(block.value[:, 0]) == 0:
+        #                 values[point.y:point.y+block.height, max(0, point.x):min(values.shape[1]+1, point.x+block.width)] += block.value[:, 1:]
+
+        #         if min(values.shape[1] + 1, point.x+block.width) == values.shape[1]+1:
+        #             if np.sum(block.value[:, -1]) == 0:
+        #                 values[point.y:point.y+block.height, max(-1, point.x):min(values.shape[1]+1, point.x+block.width)] += block.value[:, :-1]
+
+        #     else:            
+        #         if not is_overlap_:
+        #             values[point.y:point.y+block.height, point.x:point.x+block.width] += block.value
+        #         else:
+        #             # Stuck
+        #             return None, point
+
+        # # Not overlap with the projection
+        # values[values > len(Block.BLOCKS) - 1] -= len(Block.BLOCKS) - 1
+
+        return values
 
     def remove_lines(self, y=0):
+        reward = 0
         # Remove all filled rows
         y = self.board_h - 1
         while y >= 0:
@@ -129,19 +203,42 @@ class Board:
                 # Move
                 self.value[1:y+1, :] = self.value[0:y, :]
                 self.value[0, :] = np.zeros(self.board_w, dtype=np.int8)
+                reward += self.REWARD
+
             else:
                 y -= 1
+        
+        return reward
 
     def submit(self, block, point):
-        self.value[point.y:point.y+block.height, point.x:point.x+block.width] += block.value
-        self.remove_lines()
+        
+        w_s, w_e = max(0, point.x), min(self.board_w, point.x + block.width)
+        h_s, h_e = max(0, point.y), min(self.board_h, point.y + block.height)
+        
+        # Not overlap with the projection
+        if (w_e - w_s) != block.width:
+            # print('Overlap with side')
+            # print((h_s, h_e, w_s, w_e, block_identity, len(Block.BLOCKS) - 1, ))
+
+            if w_e <= point.x + block.width - 1:
+                # print(f'{w_e} == {point.x} + {block.width} - 1')
+                self.value[h_s:h_e, w_s:w_e] += block.value[:, :(block.width - (point.x + block.width - w_e))]
+                
+            if w_s == 0:
+                # print(f'{w_s}')
+                self.value[h_s:h_e, w_s:w_e] += block.value[:, (block.width - (w_e - w_s)):]
+                
+        else:
+            self.value[h_s:h_e, w_s:w_e] += block.value
+            # self.value[point.y:point.y+block.height, point.x:point.x+block.width] += block.value
+
+        return self.remove_lines()
 
     def get_projection_point(self, block, point):
         p = Point(point.x, point.y)
-        overlap = False
-        while not overlap:
+        while p.y + block.height < self.value.shape[0]:
             p.y += 1
-            if self.is_block_overlap(block, p):
+            if self.is_block_overlap(block, p, verbose=False):
                 p.y -= 1
-                overlap = True
+                break
         return p
